9e6144c854ee7df562abcd6701050adeb7b2542a
ArchimedesIX <35717155+ArchimedesIX@users.noreply.github.com>
2018-08-31 17:36:53 -0400

    Add files via upload

--- src/rpc/mining.cpp
+++ src/rpc/mining.cpp
@@ -4,6 +4,7 @@
 // Distributed under the MIT software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
+#include <auxpow.h>
 #include <base58.h>
 #include <amount.h>
 #include <chain.h>
