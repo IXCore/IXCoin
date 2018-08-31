d75abec30e09852fdb7e41b1788e6afc96571fea
ArchimedesIX <35717155+ArchimedesIX@users.noreply.github.com>
2018-08-30 22:54:26 -0400

    Add files via upload

--- src/primitives/transaction.h
+++ src/primitives/transaction.h
@@ -348,17 +348,6 @@ public:
 
     std::string ToString() const;
 
-    // Add overloaded HasWitness
-    bool HasWitness() const
-    {
-        for (size_t i = 0; i < vin.size(); i++) {
-            if (!vin[i].scriptWitness.IsNull()) {
-                return true;
-            }
-        }
-        return false;
-    }
-
 //    void UpdateHash() const;
 };
 
