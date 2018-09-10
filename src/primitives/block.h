dc102f2f457f4140abb0b5fa324da5bd658eee7c
ArchimedesIX <35717155+ArchimedesIX@users.noreply.github.com>
2018-09-10 09:40:05 -0400

    Add files via upload

--- src/primitives/block.h
+++ src/primitives/block.h
@@ -1,18 +1,14 @@
 // Copyright (c) 2009-2010 Satoshi Nakamoto
-// Copyright (c) 2009-2018 The Bitcoin Core developers
+// Copyright (c) 2009-2017 The Bitcoin Core developers
 // Distributed under the MIT software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
 #ifndef BITCOIN_PRIMITIVES_BLOCK_H
 #define BITCOIN_PRIMITIVES_BLOCK_H
 
-#include <auxpow.h>
 #include <primitives/transaction.h>
-#include <primitives/pureheader.h>
 #include <serialize.h>
 #include <uint256.h>
-
-#include <memory>
 
 /** Nodes collect new transactions into a block, hash them into a hash tree,
  * and scan through nonce values to make the block's hash satisfy proof-of-work
@@ -21,12 +17,16 @@
  * in the block is a special one that creates a new coin owned by the creator
  * of the block.
  */
-class CBlockHeader : public CPureBlockHeader
+class CBlockHeader
 {
 public:
-
-    // auxpow (if this is a merge-minded block)
-    std::shared_ptr<CAuxPow> auxpow;
+    // header
+    int32_t nVersion;
+    uint256 hashPrevBlock;
+    uint256 hashMerkleRoot;
+    uint32_t nTime;
+    uint32_t nBits;
+    uint32_t nNonce;
 
     CBlockHeader()
     {
@@ -37,29 +37,35 @@ public:
 
     template <typename Stream, typename Operation>
     inline void SerializationOp(Stream& s, Operation ser_action) {
-        READWRITEAS(CPureBlockHeader, *this);
-
-        if (this->IsAuxpow())
-        {
-            if (ser_action.ForRead())
-                auxpow = std::make_shared<CAuxPow>();
-            assert(auxpow != nullptr);
-            READWRITE(*auxpow);
-        } else if (ser_action.ForRead())
-            auxpow.reset();
+        READWRITE(this->nVersion);
+        READWRITE(hashPrevBlock);
+        READWRITE(hashMerkleRoot);
+        READWRITE(nTime);
+        READWRITE(nBits);
+        READWRITE(nNonce);
     }
 
     void SetNull()
     {
-        CPureBlockHeader::SetNull();
-        auxpow.reset();
+        nVersion = 0;
+        hashPrevBlock.SetNull();
+        hashMerkleRoot.SetNull();
+        nTime = 0;
+        nBits = 0;
+        nNonce = 0;
     }
 
-    /**
-     * Set the block's auxpow (or unset it).  This takes care of updating
-     * the version accordingly.
-     */
-    void SetAuxpow (std::unique_ptr<CAuxPow> apow);
+    bool IsNull() const
+    {
+        return (nBits == 0);
+    }
+
+    uint256 GetHash() const;
+
+    int64_t GetBlockTime() const
+    {
+        return (int64_t)nTime;
+    }
 };
 
 
@@ -80,14 +86,14 @@ public:
     CBlock(const CBlockHeader &header)
     {
         SetNull();
-        *(static_cast<CBlockHeader*>(this)) = header;
+        *((CBlockHeader*)this) = header;
     }
 
     ADD_SERIALIZE_METHODS;
 
     template <typename Stream, typename Operation>
     inline void SerializationOp(Stream& s, Operation ser_action) {
-        READWRITEAS(CBlockHeader, *this);
+        READWRITE(*(CBlockHeader*)this);
         READWRITE(vtx);
     }
 
@@ -107,7 +113,6 @@ public:
         block.nTime          = nTime;
         block.nBits          = nBits;
         block.nNonce         = nNonce;
-        block.auxpow         = auxpow;
         return block;
     }
 
