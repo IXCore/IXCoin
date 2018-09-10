5364955a15c8d5cd586d78b905aa85efadf02e32
ArchimedesIX <35717155+ArchimedesIX@users.noreply.github.com>
2018-09-10 09:37:04 -0400

    Add files via upload

--- src/primitives/block.h
+++ src/primitives/block.h
@@ -1,18 +1,18 @@
 // Copyright (c) 2009-2010 Satoshi Nakamoto
-// Copyright (c) 2009-2015 The Bitcoin Core developers
+// Copyright (c) 2009-2018 The Bitcoin Core developers
 // Distributed under the MIT software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
 #ifndef BITCOIN_PRIMITIVES_BLOCK_H
 #define BITCOIN_PRIMITIVES_BLOCK_H
 
-#include "auxpow.h"
-#include "primitives/transaction.h"
-#include "primitives/pureheader.h"
-#include "serialize.h"
-#include "uint256.h"
+#include <auxpow.h>
+#include <primitives/transaction.h>
+#include <primitives/pureheader.h>
+#include <serialize.h>
+#include <uint256.h>
 
-#include <boost/shared_ptr.hpp>
+#include <memory>
 
 /** Nodes collect new transactions into a block, hash them into a hash tree,
  * and scan through nonce values to make the block's hash satisfy proof-of-work
@@ -26,7 +26,7 @@ class CBlockHeader : public CPureBlockHeader
 public:
 
     // auxpow (if this is a merge-minded block)
-    boost::shared_ptr<CAuxPow> auxpow;
+    std::shared_ptr<CAuxPow> auxpow;
 
     CBlockHeader()
     {
@@ -36,15 +36,14 @@ public:
     ADD_SERIALIZE_METHODS;
 
     template <typename Stream, typename Operation>
-    inline void SerializationOp(Stream& s, Operation ser_action, int nType, int nVersion) {
-        READWRITE(*(CPureBlockHeader*)this);
-        nVersion = this->GetBaseVersion();
+    inline void SerializationOp(Stream& s, Operation ser_action) {
+        READWRITEAS(CPureBlockHeader, *this);
 
         if (this->IsAuxpow())
         {
             if (ser_action.ForRead())
-                auxpow.reset (new CAuxPow());
-            assert(auxpow);
+                auxpow = std::make_shared<CAuxPow>();
+            assert(auxpow != nullptr);
             READWRITE(*auxpow);
         } else if (ser_action.ForRead())
             auxpow.reset();
@@ -59,9 +58,8 @@ public:
     /**
      * Set the block's auxpow (or unset it).  This takes care of updating
      * the version accordingly.
-     * @param apow Pointer to the auxpow to use or NULL.
      */
-    void SetAuxpow (CAuxPow* apow);
+    void SetAuxpow (std::unique_ptr<CAuxPow> apow);
 };
 
 
@@ -69,7 +67,7 @@ class CBlock : public CBlockHeader
 {
 public:
     // network and disk
-    std::vector<CTransaction> vtx;
+    std::vector<CTransactionRef> vtx;
 
     // memory only
     mutable bool fChecked;
@@ -82,14 +80,14 @@ public:
     CBlock(const CBlockHeader &header)
     {
         SetNull();
-        *((CBlockHeader*)this) = header;
+        *(static_cast<CBlockHeader*>(this)) = header;
     }
 
     ADD_SERIALIZE_METHODS;
 
     template <typename Stream, typename Operation>
-    inline void SerializationOp(Stream& s, Operation ser_action, int nType, int nVersion) {
-        READWRITE(*(CBlockHeader*)this);
+    inline void SerializationOp(Stream& s, Operation ser_action) {
+        READWRITEAS(CBlockHeader, *this);
         READWRITE(vtx);
     }
 
@@ -126,16 +124,14 @@ struct CBlockLocator
 
     CBlockLocator() {}
 
-    CBlockLocator(const std::vector<uint256>& vHaveIn)
-    {
-        vHave = vHaveIn;
-    }
+    explicit CBlockLocator(const std::vector<uint256>& vHaveIn) : vHave(vHaveIn) {}
 
     ADD_SERIALIZE_METHODS;
 
     template <typename Stream, typename Operation>
-    inline void SerializationOp(Stream& s, Operation ser_action, int nType, int nVersion) {
-        if (!(nType & SER_GETHASH))
+    inline void SerializationOp(Stream& s, Operation ser_action) {
+        int nVersion = s.GetVersion();
+        if (!(s.GetType() & SER_GETHASH))
             READWRITE(nVersion);
         READWRITE(vHave);
     }
@@ -150,8 +146,5 @@ struct CBlockLocator
         return vHave.empty();
     }
 };
-
-/** Compute the consensus-critical block weight (see BIP 141). */
-int64_t GetBlockWeight(const CBlock& tx);
 
 #endif // BITCOIN_PRIMITIVES_BLOCK_H
