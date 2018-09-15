3f7e6c90431399813bafc4e6e277560efbeab4df
ArchimedesIX <35717155+ArchimedesIX@users.noreply.github.com>
2018-09-15 19:40:29 -0400

    Add files via upload

--- src/qt/test/wallettests.cpp
+++ src/qt/test/wallettests.cpp
@@ -65,8 +65,8 @@ void ConfirmSend(QString* text = nullptr, bool cancel = false)
             }
         }
         delete callback;
-//    }), &Callback::call);
-    }), SLOT(call()));
+    }), &Callback::call);
+//    }), SLOT(call()));
 }
 
 //! Send coins to address and return txid.
